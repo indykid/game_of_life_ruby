23ea7349349551da5b69437324b6672f49f78005
jarkyn <jarkyn@ymail.com>
2015-04-20 18:13:34 +0100

    adds another test, not happy with this already

--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -12,4 +12,16 @@ describe Life do
 		life = Life.new([[0, 0]])
 		expect(life.generation).to eq([])
 	end
+	it "given two non-adjacent cells returns zero life cells" do
+		life = Life.new([[0, 0], [0, 5]])
+		expect(life.generation).to eq([])
+	end
+	
+	it "given two adjacent cells returns zero life cells" do
+		life = Life.new([[0, 0], [0, 1]])
+		expect(life.generation).to eq([])
+	end
+
+	it "given a cell with two adjacent cells returns that cell" do
+	end
 end
